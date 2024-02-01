81e7383efef0886a50caf535592bc84badf555a5
ALOK SINGH <14109008alok@gmail.com>
2024-02-01 16:35:15 +0530

    New Updated

--- src/test/java/ui/Login_Kent.java
+++ src/test/java/ui/Login_Kent.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class LoginTest {
+public class Login_Kent {
 
 	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
@@ -15,9 +15,11 @@ public class LoginTest {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		// driver.findElement(By.xpath("Accept all")).click();    //It is coming from Shadow root. Need to find x path for shadow root.
-        //    element = driver.execute_script("return document.querySelector('#usercentrics-root').shadowRoot.querySelector("button[data-testid='uc-accept-all-button']")");
-        //    element.click();
+        /* element = driver.execute_script("return document.querySelector('#usercentrics-root').shadowRoot.querySelector("button[data-testid='uc-accept-all-button']")");
+        element.click(); */
 		// https://stackoverflow.com/questions/70347087/how-to-handle-the-popup-accepting-all-cookies-when-the-element-is-data-testid
+		/* JavascriptExecutor js = (JavascriptExecutor) driver;
+	    js.executeScript("document.getElementByID('element id ').click();"); */
 		driver.findElement(By.xpath("button[data-testid='uc-accept-all-button']")).click();
 		driver.findElement(By.xpath("//nav[2]//div//div[1]/p[2]/a")).click();
 		driver.findElement(By.id("//*[@id=\"mat-input-4\"]")).sendKeys("msdental");
